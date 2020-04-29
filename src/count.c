fc9d2308cb7c4c5d886d66f6a9b90a839013c3a7
ecm3321 <64050157+ecm3321@users.noreply.github.com>
2020-04-29 14:50:18 -0600

    Update count.c

--- src/count.c
+++ src/count.c
@@ -8,8 +8,32 @@
 
 int main(int argc, char* argv[])
 {
-    //
-    // TODO: your code goes here.
-    //
+size_t i = 0;
+    size_t drop = 0;
+    vote_count_t list = vc_create();
+    if (list == NULL) {
+        fprintf(stderr, OOM_MESSAGE, argv[0]);
+        return 1;
+    }
+
+    const char* line;
+    while ((line = read_line()) != NULL){
+        size_t* cntpntr =  vc_update(list, line);
+        if (i >= MAX_CANDIDATES || cntpntr == NULL){
+            drop = drop + 1;
+            fprintf(stderr, DROP_MESSAGE, argv[0], line);
+            return 2;
+        } else {
+            *cntpntr = *cntpntr + 1;
+        }
+        free(line);
+        i = i + 1;
+    }
+        vc_print(list);
+        vc_destroy(list);
+        if (i >= MAX_CANDIDATES){
+            fprintf(stderr, FINAL_MESSAGE, argv[0], drop);
+            return 2;
+        }
 }
 
