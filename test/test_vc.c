c318405a4ac5c7457e91449f853d2bdaa44cfc30
Katy-Wicks111 <64050160+Katy-Wicks111@users.noreply.github.com>
2020-04-29 20:09:48 -0500

    Update test_vc.c

--- test/test_vc.c
+++ test/test_vc.c
@@ -9,30 +9,36 @@ static void test_create_destroy(void)
     vc_destroy(vc_create());
 }
 
+
 static void test_2_candidates(void)
 {
-    size_t* cp;
+    size_t* cp;//what is cp?
     vote_count_t vc = vc_create();
     assert(vc);
 
-    CHECK_POINTER( vc_max(vc), NULL );
+    CHECK_POINTER( vc_max(vc), NULL );//what is check pointer?
     CHECK_POINTER( vc_min(vc), NULL );
     CHECK_SIZE( vc_lookup(vc, "alice"), 0 );
     CHECK_SIZE( vc_lookup(vc, "bob"), 0 );
     CHECK_SIZE( vc_total(vc), 0 );
 
-    cp = vc_update(vc, "alice");
-    CHECK( cp );
+
+    cp = vc_update(vc, "alice");//what is going on here?
+    CHECK( cp );//checking that cp is not null
     ++*cp;
     CHECK_STRING( vc_max(vc), "alice" );
     CHECK_STRING( vc_min(vc), "alice" );
     CHECK_SIZE( vc_lookup(vc, "alice"), 1 );
     CHECK_SIZE( vc_lookup(vc, "bob"), 0 );
+
     CHECK_SIZE( vc_total(vc), 1 );
 
     cp = vc_update(vc, "bob");
     CHECK( cp );
     ++*cp;
+    CHECK_STRING( vc_max(vc), "alice" );//max tie
+    CHECK_STRING( vc_min(vc), "bob" );//min tie
+
     cp = vc_update(vc, "alice");
     CHECK( cp );
     ++*cp;
@@ -42,8 +48,23 @@ static void test_2_candidates(void)
     CHECK_SIZE( vc_lookup(vc, "bob"), 1 );
     CHECK_SIZE( vc_total(vc), 3 );
 
+    cp = vc_update(vc, "bob");//here down not working
+    CHECK( cp );
+    ++*cp;
+    cp = vc_update(vc, "bob");
+    CHECK( cp );
+    ++*cp;
+    cp = vc_update(vc, "alice");
+    CHECK( cp );
+    ++*cp;
+    CHECK_STRING( vc_max(vc), "alice" );
+    CHECK_STRING( vc_min(vc), "bob" ); //end of sketchy part
+
+
+
     vc_destroy(vc);
 }
+
 
 //
 // You need more tests here.
